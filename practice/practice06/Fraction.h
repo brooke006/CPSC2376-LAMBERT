e6fd2771bb45e014f4a532d2f11cac2cf1084150
brooke006 <143116456+brooke006@users.noreply.github.com>
2025-03-05 00:27:33 -0600

    Update Fraction.h

--- practice/practice06/Fraction.h
+++ practice/practice06/Fraction.h
@@ -1,1 +1,44 @@
+#pragma once
+#ifndef FRACTION_H
+#define FRACTION_H
+
+#include <iostream>
+#include <stdexcept>
+
+class Fraction {
+protected:
+    int num; // Numerator
+    int den; // Denominator
+    void simplify(); // Simplifies the fraction using GCD
+
+public:
+    // Constructors
+    Fraction();
+    Fraction(int n, int d);
+
+    // Getters and Setters
+    int getNumerator() const;
+    int getDenominator() const;
+    void setNumerator(int n);
+    void setDenominator(int d);
+
+    // Arithmetic Operators
+    friend Fraction operator+(const Fraction& lhs, const Fraction& rhs);
+    friend Fraction operator-(const Fraction& lhs, const Fraction& rhs);
+    friend Fraction operator*(const Fraction& lhs, const Fraction& rhs);
+    friend Fraction operator/(const Fraction& lhs, const Fraction& rhs);
+
+    // Comparison Operators
+    bool operator==(const Fraction& other) const;
+    bool operator!=(const Fraction& other) const;
+    bool operator<(const Fraction& other) const;
+    bool operator>(const Fraction& other) const;
+    bool operator<=(const Fraction& other) const;
+    bool operator>=(const Fraction& other) const;
+
+    // Stream Overloading
+    friend std::ostream& operator<<(std::ostream& out, const Fraction& f);
+};
+
+#endif
 
