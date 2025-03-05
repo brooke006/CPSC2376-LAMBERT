185f0c10a66e492c046299357d68513c8e11ce7f
brooke006 <143116456+brooke006@users.noreply.github.com>
2025-03-05 00:26:25 -0600

    Update MixedFraction.cpp

--- practice/practice06/MixedFraction.cpp
+++ practice/practice06/MixedFraction.cpp
@@ -1,1 +1,53 @@
+#include "MixedFraction.h"
+
+MixedFraction::MixedFraction(int whole, int n, int d) : Fraction(n + whole * d, d) {}
+
+MixedFraction::MixedFraction(const Fraction& fraction) : Fraction(fraction) {
+    simplify();
+}
+
+std::ostream& operator<<(std::ostream& out, const MixedFraction& mf) {
+    int whole = mf.getNumerator() / mf.getDenominator();
+    int num = abs(mf.getNumerator() % mf.getDenominator());
+    int den = mf.getDenominator();
+
+    if (whole != 0)
+        out << whole;
+
+    if (num != 0) {
+        if (whole != 0) out << " ";
+        out << num << "/" << den;
+    }
+
+    if (whole == 0 && num == 0)
+        out << "0";
+
+    return out;
+}
+#include "MixedFraction.h"
+
+MixedFraction::MixedFraction(int whole, int n, int d) : Fraction(n + whole * d, d) {}
+
+MixedFraction::MixedFraction(const Fraction& fraction) : Fraction(fraction) {
+    simplify();
+}
+
+std::ostream& operator<<(std::ostream& out, const MixedFraction& mf) {
+    int whole = mf.getNumerator() / mf.getDenominator();
+    int num = abs(mf.getNumerator() % mf.getDenominator());
+    int den = mf.getDenominator();
+
+    if (whole != 0)
+        out << whole;
+
+    if (num != 0) {
+        if (whole != 0) out << " ";
+        out << num << "/" << den;
+    }
+
+    if (whole == 0 && num == 0)
+        out << "0";
+
+    return out;
+}
 
